83ce25e5cd451dc63202659711a5cc8cebc8ef8d
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2022-05-12 21:04:38 -0600

    Prototype integration of Emotiv API
    -imported Emotiv Plugin
    -started recreating features from Unity example
    -established connection to cortex api
    -subscribed to available headset data

---
+++ Assets/BCI Integration/Emotiv/Scripts/DataSubscriber.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EmotivUnityPlugin;
+
+/// <summary>
+/// In progress - subscribes to data streams from the data stream manager
+/// </summary>
+public class DataSubscriber : MonoBehaviour
+{
+    DataStreamManager dataStream = DataStreamManager.Instance;
+
+    float dataUpdateTimer = 0;
+    const float DATA_UPDATE_INTERVAL = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isActiveAndEnabled)
+            return;
+
+        dataUpdateTimer += Time.deltaTime;
+        if (dataUpdateTimer < DATA_UPDATE_INTERVAL)
+            return;
+
+        dataUpdateTimer -= DATA_UPDATE_INTERVAL;
+
+        // update data
+    }
+}
