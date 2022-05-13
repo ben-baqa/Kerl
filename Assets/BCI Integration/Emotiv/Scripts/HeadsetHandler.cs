c4817ee5edabb8a8916bd70e9f369536a0d33864
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2022-05-13 17:50:19 -0600

    Successfully detected mental commands in Unity

--- Assets/BCI Integration/Emotiv/Scripts/HeadsetHandler.cs
+++ Assets/BCI Integration/Emotiv/Scripts/HeadsetHandler.cs
@@ -10,11 +10,25 @@ using System;
 public class HeadsetHandler : MonoBehaviour
 {
     DataProcessing data = DataProcessing.Instance;
+    BCITraining training;
 
+    string headsetID;
 
     void Start()
     {
+        training = new BCITraining();
+        training.Init();
         data.onHeadsetChange += OnHeadsetChanged;
+        data.HeadsetConnected += OnConnect;
+        data.HeadsetConnectFail += OnConnectFail;
+        TrainingHandler.Instance.QueryProfileOK += OnProfileQuery;
+    }
+
+    private void OnDestroy()
+    {
+        data.onHeadsetChange -= OnHeadsetChanged;
+        data.HeadsetConnected -= OnConnect;
+        data.HeadsetConnectFail -= OnConnectFail;
     }
 
     private void OnHeadsetChanged(object sender, EventArgs args)
@@ -33,8 +47,51 @@ public class HeadsetHandler : MonoBehaviour
                 continue;
             }
 
-            print("Headset Detected!");
-            print(item.Value);
+            print($"Headset Detected!   ID: ${item.Value.HeadsetID}");
+            headsetID = item.Value.HeadsetID;
         }
+    }
+
+    private void OnConnect(object sender, string args)
+    {
+        print("====== HEADSET CONNECTION SUCCESS");
+    }
+
+    private void OnConnectFail(object sender, string args)
+    {
+        print("====== HEADSET CONNECTION FAILURE");
+    }
+
+    public void ConnectionTest()
+    {
+        List<string> dataStreamList = new List<string>() { DataStreamName.SysEvents, DataStreamName.MentalCommands };
+        DataStreamManager.Instance.StartDataStream(dataStreamList, headsetID);
+
+        DataStreamManager.Instance.MentalCommandReceived += OnMentalCommandRecieved;
+    }
+
+    public void LoadProfile(string profileName)
+    {
+        training.QueryProfile();
+        profileName = "test";
+        training.LoadProfile(profileName);
+
+    }
+
+    public void OnMentalCommandRecieved(object sender, MentalCommandEventArgs args)
+    {
+        print("------------------------------------------------");
+        print("------------------------------------------------");
+        print("--------------YEET---------------");
+        print("------------------------------------------------");
+        print("------------------------------------------------");
+    }
+
+    private void OnProfileQuery(object sender, List<string> args)
+    {
+        print("------------------------------------------------");
+        foreach (var i in training.ProfileLists)
+            print(i);
+        print("------------------------------------------------");
     }
 }
