6733f2c58c5b3689bd94e6b968bf954ba50976ee
SADragonMaker <44631569+SADragonMaker@users.noreply.github.com>
2022-07-05 20:58:23 -0600

    test thrower with curve

--- Assets/Scripts/Gameplay/Gameplay changes/TestNewRock.cs
+++ Assets/Scripts/Gameplay/Gameplay changes/TestNewRock.cs
@@ -14,26 +14,27 @@ public class TestNewRock : MonoBehaviour
 
     private Vector3 velocity;
     private float traveled = 0;
+    private bool thrown = false;
     private bool inOrbit = true;
 
-    private void Start()
-    {
-        velocity = MidPoint.normalized * (Threshold - MakeShiftBezierArclength(100) * Mathf.Log(Decay));
-    }
 
     private void FixedUpdate()
     {
-        traveled += velocity.magnitude * Time.deltaTime;
-        GetComponent<Rigidbody>().MovePosition(transform.position + velocity * Time.deltaTime);
-        velocity *= Mathf.Pow(Decay, Time.deltaTime);
-        if (inOrbit)
-        { 
-            Vector3 destination = Vector3.Lerp(MidPoint, EndPoint, traveled / MakeShiftBezierArclength(100));
-            velocity = (destination - transform.localPosition).normalized * velocity.magnitude;
-        }
+        if (thrown)
+        {
+            traveled += velocity.magnitude * Time.deltaTime;
+            GetComponent<Rigidbody>().MovePosition(transform.position + velocity * Time.deltaTime);
+            velocity *= Mathf.Pow(Decay, Time.deltaTime);
+            if (inOrbit)
+            {
+                Vector3 destination = Vector3.Lerp(MidPoint, EndPoint, traveled / MakeShiftBezierArclength(20));
+                velocity = (destination - transform.localPosition).normalized * velocity.magnitude;
+            }
 
-        if (velocity.magnitude <= Threshold) {
-            velocity = Vector3.zero;
+            if (velocity.magnitude <= Threshold)
+            {
+                velocity = Vector3.zero;
+            }
         }
     }
 
@@ -49,43 +50,17 @@ public class TestNewRock : MonoBehaviour
         return ret;
     }
 
+    public void Throw() {
+        velocity = MidPoint.normalized * (Threshold - MakeShiftBezierArclength(100) * Mathf.Log(Decay));
+        thrown = true;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         GameObject target = collision.gameObject;
         if (target.GetComponent<TestNewRock>() != null) {
             velocity *= -5.0f;
             inOrbit = false;
-        }
-    }
-}
-
-[CustomEditor(typeof(TestNewRock))]
-public class TestNewRockEditor : Editor
-{
-    private TestNewRock rock;
-    private Transform handleTransform;
-
-    private void OnSceneGUI()
-    {
-        rock = target as TestNewRock;
-        handleTransform = rock.transform;
-
-        Vector3 p0 = handleTransform.position;
-        Vector3 p1 = handleTransform.position + rock.MidPoint;
-        Vector3 p2 = handleTransform.position + rock.EndPoint;
-
-        Handles.color = Color.white;
-        Handles.DrawLine(p0, p1);
-        Handles.color = Color.cyan;
-        Handles.DrawLine(p1, p2);
-
-        Handles.color = Color.red;
-        int steps = 100;
-        Vector3 lastPoint = p0;
-        for (int i = 1; i <= steps; i++) {
-            Vector3 temp = Bezier.GetPoint(p0, p1, p2, (float)i / (float)steps);
-            Handles.DrawLine(lastPoint, temp);
-            lastPoint = temp;
         }
     }
 }
--- Assets/Scripts/Gameplay/Gameplay changes/TestThrower.cs
+++ Assets/Scripts/Gameplay/Gameplay changes/TestThrower.cs
@@ -2,8 +2,131 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEditor;
 
 public class TestThrower : MonoBehaviour
 {
-    
+    enum ThrowState { 
+        SelectEnd,
+        SelectMid,
+        PreThrow,
+        PostThrow
+    }
+
+    public Vector3 ForwardDirection;
+    public Vector3 AimingWidth;
+
+    public LineRenderer CurveRenderer;
+    public TestNewRock rock;
+
+    [Range(-1.0f, 1.0f)]
+    public float EndPointAim;
+    [Range(-1.0f, 1.0f)]
+    public float MidpointAim;
+
+    private bool aimMovingRight = true;
+    private ThrowState throwState = ThrowState.SelectEnd;
+
+    public void CurveUpdate() {
+        int steps = 20;
+        for (int i = 0; i <= steps; i++)
+        {
+            CurveRenderer.SetPosition(i, Bezier.GetPoint(Vector3.zero, GetMidPoint(), GetEndPoint(), (float)i / (float)steps));
+        }
+    }
+
+    private void Start()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        if (throwState == ThrowState.SelectEnd)
+        {
+            EndPointAim += Time.deltaTime * (aimMovingRight ? 1 : -1);
+            if (Mathf.Abs(EndPointAim) >= 1)
+            {
+                EndPointAim = aimMovingRight ? 1 : -1;
+                aimMovingRight = !aimMovingRight;
+            }
+            if (Keyboard.current[Key.Space].wasPressedThisFrame) {
+                throwState = ThrowState.SelectMid;
+                aimMovingRight = true;
+            }
+            CurveUpdate();
+        }
+        else if (throwState == ThrowState.SelectMid)
+        {
+            MidpointAim += Time.deltaTime * (aimMovingRight ? 1 : -1);
+            if (Mathf.Abs(MidpointAim) >= 1)
+            {
+                MidpointAim = aimMovingRight ? 1 : -1;
+                aimMovingRight = !aimMovingRight;
+            }
+            if (Keyboard.current[Key.Space].wasPressedThisFrame)
+            {
+                throwState = ThrowState.PreThrow;
+            }
+            CurveUpdate();
+        }
+        else if (throwState == ThrowState.PreThrow)
+        {
+            if (Keyboard.current[Key.Space].wasPressedThisFrame)
+            {
+                throwState = ThrowState.PostThrow;
+                rock.MidPoint = GetMidPoint();
+                rock.EndPoint = GetEndPoint();
+                rock.Throw();
+            }
+        }
+    }
+
+    public Vector3 GetEndPoint() {
+        return ForwardDirection + AimingWidth * EndPointAim;
+    }
+
+    public Vector3 GetMidPoint()
+    {
+        return ForwardDirection * 0.5f + AimingWidth * MidpointAim;
+    }
+}
+
+[CustomEditor(typeof(TestThrower))]
+public class TestThrowerEditor : Editor
+{
+    private TestThrower thrower;
+    private Transform handleTransform;
+
+    private void OnSceneGUI()
+    {
+        thrower = target as TestThrower;
+        handleTransform = thrower.transform;
+
+        Vector3 direction = handleTransform.position + thrower.ForwardDirection;
+
+        Vector3 start = handleTransform.position;
+        Vector3 mid = handleTransform.position + thrower.GetMidPoint();
+        Vector3 end = handleTransform.position + thrower.GetEndPoint();
+
+        Handles.color = Color.blue;
+        Handles.DrawLine(start, direction);
+        Handles.color = Color.green;
+        Handles.DrawLine(direction + thrower.AimingWidth, direction - thrower.AimingWidth);
+
+        Handles.color = Color.white;
+        Handles.DrawLine(start, mid);
+        Handles.color = Color.cyan;
+        Handles.DrawLine(mid, end);
+
+        Handles.color = Color.red;
+        int steps = 20;
+        Vector3 lastPoint = start;
+        for (int i = 1; i <= steps; i++)
+        {
+            Vector3 temp = Bezier.GetPoint(start, mid, end, (float)i / (float)steps);
+            Handles.DrawLine(lastPoint, temp);
+            lastPoint = temp;
+        }
+        thrower.CurveUpdate();
+    }
 }
